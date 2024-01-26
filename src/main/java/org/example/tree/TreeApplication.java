02d4a55dc89c3c871b047da1d9d42bea1d4f9390
구환준/모건 <koojun99@naver.com>
2024-01-26 14:47:11 +0900

    Initial commit

---
+++ src/main/java/org/example/tree/TreeApplication.java
@@ -0,0 +1,13 @@
+package org.example.tree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tree/TreeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
