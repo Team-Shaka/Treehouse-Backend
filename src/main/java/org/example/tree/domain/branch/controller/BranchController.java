ed9f6154e312b4e228a1db70acf06c439eece188
구환준/모건 <koojun99@naver.com>
2024-03-02 18:03:33 +0900

    [FEAT]/#55-특정 멤버와의 브랜치 뷰 구현

--- src/main/java/org/example/tree/domain/branch/controller/BranchController.java
+++ src/main/java/org/example/tree/domain/branch/controller/BranchController.java
@@ -1,4 +1,24 @@
 package org.example.tree.domain.branch.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.tree.domain.branch.dto.BranchResponseDTO;
+import org.example.tree.domain.branch.service.BranchService;
+import org.example.tree.global.common.ApiResponse;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
 public class BranchController {
+
+    private final BranchService branchService;
+
+    @GetMapping("/trees/{treeId}/branchView")
+    public ApiResponse<BranchResponseDTO.branchView> getBranchView(
+            @PathVariable Long treeId,
+            @RequestParam("memberId") Long profileId,
+            @RequestHeader("Authorization") final String header
+    ) {
+        String token = header.replace("Bearer ", "");
+        return ApiResponse.onSuccess(branchService.getBranchView(treeId, token, profileId));
+    }
 }
--- src/main/java/org/example/tree/domain/branch/dto/BranchResponseDTO.java
+++ src/main/java/org/example/tree/domain/branch/dto/BranchResponseDTO.java
@@ -1,4 +1,55 @@
 package org.example.tree.domain.branch.dto;
 
+import lombok.*;
+
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+
 public class BranchResponseDTO {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ShortestPathResult{
+        private int distance;
+        private List<Long> path;
+    }
+
+    // Node 정보를 담을 클래스
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class NodeDTO {
+        private Long id;
+        private String profileImageUrl;
+        private String memberName;
+
+        // 생성자, getter, setter 생략
+    }
+
+    // Link 정보를 담을 클래스
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class LinkDTO {
+        private Long sourceId;
+        private Long targetId;
+
+        // 생성자, getter, setter 생략
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class branchView{
+        private List<NodeDTO> nodes;
+        private List<LinkDTO> links;
+        private Long startId;
+        private Long endId;
+    }
 }
