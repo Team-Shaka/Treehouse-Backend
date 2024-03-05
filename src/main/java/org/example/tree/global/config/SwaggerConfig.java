cac8e98be5c80f910386bdd70c6e35b5259392a4
구환준/모건 <koojun99@naver.com>
2024-03-05 14:50:40 +0900

    [REFACTOR]/#60-PostImage 생성 로직 수정

--- src/main/java/org/example/tree/global/config/SwaggerConfig.java
+++ src/main/java/org/example/tree/global/config/SwaggerConfig.java
@@ -5,7 +5,10 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+<<<<<<< Updated upstream
 import io.swagger.v3.oas.models.servers.Server;
+=======
+>>>>>>> Stashed changes
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,16 +27,17 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        SecurityScheme securityScheme = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
-                .in(SecurityScheme.In.HEADER).name("Authorization");
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+            SecurityScheme securityScheme = new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                    .in(SecurityScheme.In.HEADER).name("Authorization");
+            SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
 
-        return new OpenAPI()
-                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
-                .security(Arrays.asList(securityRequirement))
-                .servers(Arrays.asList(
-                        new Server().url("https://dev.tttree.shop").description("Develop server")));
+            return new OpenAPI()
+                    .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                    .security(Arrays.asList(securityRequirement))
+                    .info(new Info()
+                        .title("Tree API")
+                        .version("1.0.0"));
     }
 
 }
