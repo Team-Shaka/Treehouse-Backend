9e9c2cbb2b7be05d4d88dbd3501edf25ae1ccbd9
구환준/모건 <koojun99@naver.com>
2024-04-02 19:30:42 +0900

    [REFACTOR]/#83-Spring Security 관련 코드들 이해하기

--- src/main/java/org/example/tree/global/security/handler/annotation/resolver/AuthMemberArgumentResolver.java
+++ src/main/java/org/example/tree/global/security/handler/annotation/resolver/AuthMemberArgumentResolver.java
@@ -19,9 +19,15 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 @Component
 @RequiredArgsConstructor
 public class AuthMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    /**
+     * supportsParameter
+     * - 해당 파라미터를 지원하는지 여부를 반환
+     * - AuthMember 어노테이션이 없거나, Member 타입이 아닌 경우 false 반환
+     */
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        AuthMember authUser = parameter.getParameterAnnotation(AuthMember.class);
+        AuthMember authUser = parameter.getParameterAnnotation(AuthMember.class); // 메서드 파라미터에서 @AuthMember 찾기
         if (authUser == null) return false;
         if (parameter.getParameterType().equals(Member.class) == false) {
             return false;
@@ -29,6 +35,12 @@ public class AuthMemberArgumentResolver implements HandlerMethodArgumentResolver
         return true;
     }
 
+    /**
+     * resolveArgument
+     * 실제로 파라미터의 값을 해석해주는 메서드
+     *  파라미터에 전달할 객체를 반환
+     * - SecurityContextHolder에서 인증 객체를 가져와서 Member 객체로 변환하여 반환
+     */
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         Object principal = null;
@@ -37,7 +49,7 @@ public class AuthMemberArgumentResolver implements HandlerMethodArgumentResolver
         if (authentication != null) {
             principal = authentication.getPrincipal();
         }
-        if (principal == null || principal.getClass() == String.class) {
+        if (principal == null || principal.getClass() == String.class) { //Authentication 객체가 null이거나, principal이 String 타입('anonymousUser')인 경우
             throw new GeneralException(GlobalErrorCode.MEMBER_NOT_FOUND);
         }
 
